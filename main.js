c3652083bef4575ccbf8926ed64e456d58d96bad
3meraldK <ooyaxcontact@gmail.com>
2023-08-22 21:56:06 +0200

    Fixes, optimization, rework, QoL

--- main.js
+++ main.js
@@ -1,72 +1,89 @@
-function isEmpty(value) {
-	if (value.value.replaceAll(' ', '').length == 0) return true;
+const tab = '&nbsp;&nbsp;&nbsp;&nbsp;';
+
+function isEmpty(input) {
+	if (input.replaceAll(' ', '').length == 0) return true;
 	return false;
 }
 
-function updateFormable() {
+function updateOutput() {
 	
 	const output = document.getElementById('output-text'),
-		name = document.getElementById('name'),
-		canform = document.getElementById('canform'),
-		required = document.getElementById('required'),
-		tiles = document.getElementById('tiles'),
-		exclusive = document.getElementById('exclusive'),
-		buttonName = document.getElementById('button-name'),
-		buttonDesc = document.getElementById('button-desc'),
-		alertTitle = document.getElementById('alert-title'),
-		alertDesc = document.getElementById('alert-desc'),
-		alertButton = document.getElementById('alert-button'),
-		stabilityGain = document.getElementById('stability-gain'),
-		flagLink = document.getElementById('flag-link'),
+		name = document.getElementById('name').value,
+		canform = document.getElementById('canform').value,
+		required = document.getElementById('required').value,
+		tiles = document.getElementById('tiles').value,
+		exclusive = document.getElementById('exclusive').value,
+		buttonName = document.getElementById('button-name').value,
+		buttonDesc = document.getElementById('button-desc').value,
+		alertTitle = document.getElementById('alert-title').value,
+		alertDesc = document.getElementById('alert-desc').value,
+		alertButton = document.getElementById('alert-button').value,
+		stabilityGain = document.getElementById('stability-gain').value,
+		flagLink = document.getElementById('flag-link').value,
 
-		canformlist = formListString(canform, ''),
-		requiredlist = formListString(required, ''),
-		tilelist = formListString(tiles, ''),
-		exclusivelist = formListString(exclusive, '');
+		canformlist = formListString(canform),
+		requiredlist = formListString(required),
+		tilelist = formListString(tiles),
+		exclusivelist = formListString(exclusive);
 
 
 	if (!isEmpty(name) && !isEmpty(canform) && !isEmpty(required) && !isEmpty(buttonName) && !isEmpty(buttonDesc)) {
 
-		copyVisibility('visible');
 		const isAlertVisible = (!isEmpty(alertTitle) || !isEmpty(alertDesc) || !isEmpty(alertButton));
+		buttonVisibility('visible');
 		output.innerHTML = 
-		`{<br>&nbsp;&nbsp;&nbsp;&nbsp;FormableName = "${name.value}",
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;CountriesCanForm = {${canformlist}},
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;RequiredCountries = {${requiredlist}},
-		${isEmpty(tiles) ? '' : `<br>&nbsp;&nbsp;&nbsp;&nbsp;RequiredTiles = {${tilelist}},`}
-		${isEmpty(exclusive) ? '' : `<br>&nbsp;&nbsp;&nbsp;&nbsp;ExclusiveFormables = {${exclusivelist}},`}
-		<br><br>&nbsp;&nbsp;&nbsp;&nbsp;FormableButton = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ButtonName = "${buttonName.value}",
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ButtonDescription = "${buttonDesc.value}",
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;},
-		${!isAlertVisible ? '' : `<br><br>&nbsp;&nbsp;&nbsp;&nbsp;CustomAlert = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Title = "${isEmpty(alertTitle) ? '' : alertTitle.value}",
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Desc = "${isEmpty(alertDesc) ? '' : alertDesc.value}",
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Button = "${isEmpty(alertButton) ? '' : alertButton.value}",<br>&nbsp;&nbsp;&nbsp;&nbsp;},`}
-		${isEmpty(stabilityGain) ? '' : `<br><br>&nbsp;&nbsp;&nbsp;&nbsp;CustomAttributes = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;["Stability_Gain"] = ${stabilityGain.value},
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;},`}
-		<br>},${isEmpty(flagLink) ? '' : '<br><br>Flag URL: ' + flagLink.value}`;
+		`{<br>${tab}FormableName = "${name}",
+		<br>${tab}CountriesCanForm = {${canformlist}},
+		<br>${tab}RequiredCountries = {${requiredlist}},
+		${isEmpty(tiles) ? '' : `<br>${tab}RequiredTiles = {${tilelist}},`}
+		${isEmpty(exclusive) ? '' : `<br>${tab}ExclusiveFormables = {${exclusivelist}},`}
+		<br><br>${tab}FormableButton = {<br>${tab}${tab}ButtonName = "${buttonName}",
+		<br>${tab}${tab}ButtonDescription = "${buttonDesc}",
+		<br>${tab}},
+		${!isAlertVisible ? '' : `<br><br>${tab}CustomAlert = {<br>${tab}${tab}Title = "${isEmpty(alertTitle) ? '' : alertTitle}",
+		<br>${tab}${tab}Desc = "${isEmpty(alertDesc) ? '' : alertDesc}",
+		<br>${tab}${tab}Button = "${isEmpty(alertButton) ? '' : alertButton}",<br>${tab}},`}
+		${isEmpty(stabilityGain) ? '' : `<br><br>${tab}CustomAttributes = {<br>${tab}${tab}["Stability_Gain"] = ${stabilityGain},
+		<br>${tab}},`}
+		<br>},${isEmpty(flagLink) ? '' : '<br><br>Link to the flag: ' + flagLink}`;
 
 	} else {
-		output.innerHTML = 'Fill out required labels to proceed (marked by asterisks). Labels in italic are optional.';
-		copyVisibility('hidden');
+		output.innerHTML = 'Fill in the required labels to proceed (marked with asterisks). Italic labels are optional.';
+		buttonVisibility('hidden');
 	}
+
 }
 
-function formListString(input, newlist) {
-	for (const nation of input.value.split(',')) newlist = `${newlist}, "${nation.trim()}"`;
+function formListString(input) {
+	let newlist = '';
+	for (const nation of input.split(',')) newlist = `${newlist}, "${nation.trim()}"`;
 	return newlist.replace(', ', '');
 }
 
 function copyOutput() {
-	const output = document.getElementById('output-text');
-	if (output.innerHTML.length < 87) return;
+
+	const output = document.getElementById('output-text'),
+		buttonText = document.getElementById('copy-output-label'),
+		button = document.getElementById('copy-output');
+	if (output.innerHTML.length <= 91) return;
 	navigator.clipboard.writeText(output.innerText);
+
+	buttonText.innerText = 'Copied!';
+	button.style.backgroundColor = '#2f423a';
+	setTimeout(() => {
+		buttonText.innerText = 'Copy to clipboard';
+		button.removeAttribute('style');
+	}, 1000);
+
 }
 
-function copyVisibility(text) {
-	document.getElementById('copy-output').style.visibility = text;
+function buttonVisibility(state) {
+	document.getElementById('copy-output').style.visibility = state;
 }
 
 window.onload = function() {
-	updateFormable();
-	document.getElementById('copy-output').addEventListener('click', copyOutput);
-};
+	updateOutput();
+	document.getElementById('copy-output').onclick = copyOutput;
+}
+
+document.querySelectorAll('input').forEach(input => input.oninput = updateOutput);
